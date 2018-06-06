a386553de4f9d6beda047cc9575955c5e7a7970b
py-john <pyjohn3@gmail.com>
2018-06-05 23:57:36 -0700

    Remove 2nd workout

--- workout.py
+++ workout.py
@@ -1,20 +1,17 @@
 #!/usr/bin/env python3
 '''workout.py
    Opens a different video file for a daily
-   workout depending on which day it is run.
+   workout based on a workout calendar.
 '''
-import os
-import subprocess
-import pyautogui
 from time import sleep
 from datetime import datetime, date
+import create
 
 PROGRAM_START_DATE = datetime(2018, 3, 19, 4, 30)
-P90X_FOLDER = '/Users/john/Documents/P90X3/'
 
 
 def print_calendar(day):
-'''Draws a calendar with X's for completed days'''
+    '''Draws a calendar with X's for completed days'''
     print()
     for d in range(1, 91):
         text = d
@@ -36,8 +33,9 @@ def print_calendar(day):
             if d % 7 == 0:
                 print()
 
-def main():
 
+def main():
+    workouts = create.workout_list()
     # find workout for current day
     now = datetime.now()
     day_number = (now - PROGRAM_START_DATE).days + 1
@@ -47,15 +45,6 @@ def main():
         if day_number in w.days:
             workout = w
             break
-
-    # Check for days with 2 workouts
-    if workout == complex_upper:
-        option = None
-        print('1) Ab Ripper     2) Complex Upper')
-        while(option not in ['1', '2']):
-            option = input('> ')
-        if option == '1':
-            workout = ab_ripper
 
     # Draw calander
     print_calendar(day_number)
@@ -67,6 +56,7 @@ def main():
         sleep(2)
         workout.load_image()
         workout.load_video()
+
 
 if __name__ == '__main__':
     main()
