81f4ac8862e0ec3ffe3bb88238eccbb691fe22ea
py-john <pyjohn3@gmail.com>
2018-06-07 23:48:20 -0700

    Docstrings and formatting

--- create.py
+++ create.py
@@ -5,10 +5,9 @@ import pyautogui
 
 P90X_FOLDER = '/Users/john/Documents/P90X3/'
 
+
 class Workout(object):
-    '''Defines the workout with days to be done and
-       relavant file paths.
-    '''
+    """Define the workout with days to be done and relavant file paths."""
     def __init__(self, name, days, video_file, image_file=None):
         self.name = name
         self.days = days
@@ -33,20 +32,20 @@ class Workout(object):
 
 
 def workout_list():
-    # define workouts and add to workouts list
+    """Create and return full list of workouts."""
     workouts = []
-
     workouts.append(Workout('Total Synergistics',
                             [1, 8, 15, 64, 78],
                             P90X_FOLDER + 'Total Synergistics.mp4',
-                            P90X_FOLDER + 'Total Synergistics.png')
+                            P90X_FOLDER + 'Total Synergistics.png'))
 
     workouts.append(Workout('Agility X',
                             [2, 9, 16, 26, 65, 79, 113],
                             P90X_FOLDER + 'Agility X.mp4'))
 
     workouts.append(Workout('X3 Yoga',
-                            [3, 10, 17, 27, 31, 38, 45, 55, 59, 66, 73, 80, 86, 94, 101, 108, 114],
+                            [3, 10, 17, 27, 31, 38, 45, 55, 59,
+                             66, 73, 80, 86, 94, 101, 108, 114],
                             P90X_FOLDER + 'X3 Yoga.mp4'))
 
     workouts.append(Workout('The Challenge',
@@ -63,12 +62,13 @@ def workout_list():
                             P90X_FOLDER + 'Incinerator.png'))
 
     workouts.append(Workout('Dynamix',
-                            [7, 14, 21, 23, 28, 35, 42, 49, 51, 56, 63, 70, 77, 84, 90, 98, 105, 112, 118],
+                            [7, 14, 21, 23, 28, 35, 42, 49, 51, 56,
+                             63, 70, 77, 84, 90, 98, 105, 112, 118],
                             P90X_FOLDER + 'Dynamix.mp4'))
 
     workouts.append(Workout('Eccentric Upper',
                             [29, 32, 36, 39, 43, 46, 57, 60, 71, 74, 89],
-                            P90X_FOLDER + 'Eccentric Upper.mp4')
+                            P90X_FOLDER + 'Eccentric Upper.mp4'))
 
     workouts.append(Workout('Eccentric Lower',
                             [30, 33, 37, 40, 44, 47, 58, 61, 72, 75, 88],
